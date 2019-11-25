dbfdfcf37344bdc8323b075258f2247dc0358c27
HikanZ <hitoshi.kanashiro@gmail.com>
2019-11-25 09:48:38 -0300

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "localhost";
-$dBUsername = "root";
-$dBPassword = "";
-$dBName = "auditoriahca";
+$servername = "us-cdbr-iron-east-05.cleardb.net";
+$dBUsername = "bf4c680fa2b474";
+$dBPassword = "44085d68";
+$dBName = "heroku_b1e74900a9ac80d";
 
 $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
 

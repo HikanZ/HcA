5ee439de848119fcf99a6f634662d75fa7f07d7e
HikanZ <hitoshi.kanashiro@gmail.com>
2020-03-16 17:50:51 -0300

    Add files via upload

---
+++ inc/links.php
@@ -0,0 +1,46 @@
+<?php
+  $linkmenu = "window.location.href='index.php'";
+  $linkusers = "window.location.href='usuarios.php'";
+  $linksystem = "window.location.href='sistema.php'";
+  $linkrop = "window.location.href='rop.php'";
+  $linkaudit = "window.location.href='audit.php'";
+  $linkreport = "window.location.href='relatorio.php'";
+  $linkacc = "window.location.href='minha-acc.php'";
+  $linksetor = "window.location.href='setor.php'";
+
+	$linkadduser = "window.location.href='usuarios-cadastro.php'";
+	$linksearchuser = "window.location.href='usuarios-pesquisa.php'";
+	$linkaccessuser = "window.location.href='usuarios-acesso.php'";
+  $linkaccvisualize = "window.location.href='visualizar-acc.php'";
+  $linkuseraccess = 'usuarios-acesso-email.php';
+
+  $linkropadd = "window.location.href='rop-ano.php'";
+  $linkropremove = "window.location.href='rop-remove.php'";
+  $linkroplist = "window.location.href='rop-list.php'";
+
+  $linkropgroup = 'rop-grupo.php';
+  $linkropfinal = 'rop-final.php';
+
+  $linksetoradd = "window.location.href='setor-cadastro.php'";
+  $linksetorremove = "window.location.href='setor-remove.php'";
+  $linksetorlist = "window.location.href='setor-lista.php'";
+
+  $linksetoraddp = '';
+
+  $linkaccavatar = "window.location.href='minha-acc-avatar.php'";
+  $linkaccpassword = "window.location.href='minha-acc-senha.php'";
+
+  $linkpages = "window.location.href='asdlkf.php'";
+  $linkdatabase = "window.location.href='404.php'";
+  $linknumbers = "window.location.href='geral.php'";
+
+  $linkyear = "window.location.href='relatorio-anual.php'";
+  $linkmonth = "window.location.href='relatorio-mensal.php'";
+  $linkanswers = "window.location.href='404.php'";
+
+  $linkreportyear = "window.location.href='relatorio-anual.php'";
+  $linkreportmonth = "window.location.href='relatorio-mensal.php'";
+  $linkreportbetween = "window.location.href='relatorio-intervalo.php'";
+  $linkreporthistoric = "window.location.href='relatorio-historico.php'";
+  $linkreportaudit = "window.location.href='relatorio-audit.php'";
+?>
